4ab11dedc2da81828dd9f326240db3f836681fcb
decoder <decoder@lilve.de>
2021-01-08 13:59:15 +0100

    formatting

--- k8s/storage-pv-pvc/0-prerequisites.sh
+++ k8s/storage-pv-pvc/0-prerequisites.sh
@@ -10,4 +10,5 @@ echo $AKS_PERS_STORAGE_ACCOUNT_NAME
 echo $STORAGEKEY
 
 # Create secret to enable the pod to mount the PVC
-kubectl create secret generic azure-secret --from-literal=azurestorageaccountname=$AKS_PERS_STORAGE_ACCOUNT_NAME --from-literal=azurestorageaccountkey=$STORAGEKEY
+kubectl create secret generic azure-secret --from-literal=azurestorageaccountname=$AKS_PERS_STORAGE_ACCOUNT_NAME \
+                                           --from-literal=azurestorageaccountkey=$STORAGEKEY
